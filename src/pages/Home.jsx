2f6166dce23f60a2e3da42e1dc9e7572ecfbcc0e
Juskinbo <juskinbo@outlook.com>
2024-05-16 02:47:51 +0800

    feat: add new Component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,13 @@
+import { Flex } from '@radix-ui/themes'
+import InfoCard from '../components/InfoCard'
+
 const Home = () => {
   return (
-    <>
-      {/* <span className='text-red-500'>1111</span> */}
-    </>
+    <Flex className='h-screen'>
+      <Flex className='items-center justify-center'>
+        <InfoCard className='mx-4 sm:mx-16' />
+      </Flex>
+    </Flex>
   )
 }
 
