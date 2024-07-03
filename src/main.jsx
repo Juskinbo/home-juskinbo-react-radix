5b2c292bb12f173980d34ac712883efa14234ea8
Juskinbo <juskinbo@outlook.com>
2024-07-03 15:29:42 +0800

    feat: add animate.css & qweather-icons

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './router'
+import 'animate.css'
+import 'qweather-icons/font/qweather-icons.css'
 import './index.css'
 import '@radix-ui/themes/styles.css'
 
