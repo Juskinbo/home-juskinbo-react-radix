e2ea17db8088c6704a7a754f79c32046ffd7e59f
Juskinbo <juskinbo@outlook.com>
2024-10-04 02:41:34 +0800

    fix: change the order of style import

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,8 @@ import { RouterProvider } from 'react-router-dom'
 import { router } from './router'
 import 'animate.css'
 import 'qweather-icons/font/qweather-icons.css'
-import './index.css'
 import '@radix-ui/themes/styles.css'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
